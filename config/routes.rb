685763f8a808134b7300dd53e5227232062916b1
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-09-26 12:50:26 +0900

    make-gruop

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:new, :create, :edit, :update, :index] do
     resources :messages, only: [:index, :create]
   end
 end
