cae170d5a16e473f8db628585a6ba85215ac0c60
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-10-06 15:44:01 +0900

    middle

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,18 @@
-$(function(){
+$(document).on('turbolinks:load',function() {
+  function buildHTML(message) {
+    const img = message.image? `<img src = "${message.image}">`: "" ;
+    const html = `<div class="message" data-message-id = "${message.id}">
+                    <div class="message__upper-info">
+                      <div class="message__upper-info__talker">${message.user_name}</div>
+                      <div class="message__upper-info__date">${message.date}</div>
+                    </div>
+                    <div class="message__text">
+                      <p class="lower-message__content">${message.content}</p>
+                      ${img}
+                    </div>
+                  </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +24,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#new_message')[0].reset();
     })
   })
 })
