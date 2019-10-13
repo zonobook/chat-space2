08feb98fc8414e2d117b9b33ef8ca3ffdeac360e
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-10-13 22:44:59 +0900

    fin

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,10 +6,21 @@ $(document).on('turbolinks:load',function(){
                 </div>`
     $('#user-search-result').append(html)
   }
+
   function appendErrMsgToHtml(msg){
-    var errmsg = `<div class="chat-group-user clearfix">${msg}</div>`
-    $('#user-search-result').append(errmsg)
+    var html = `<div class="chat-group-user clearfix">${msg}</div>`
+    $('#user-search-result').append(html)
   }
+
+  function appendAddUser(user_name, user_id){
+    var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    $('#chat-group-users').append(html)
+  }
+
   $('#user-search-field').on("keyup", function(e){
     e.preventDefault();
     // ここで中身を空にする
@@ -40,5 +51,15 @@ $(document).on('turbolinks:load',function(){
         alert('ユーザー検索に失敗しました')
       })
     }
+  })
+  $('#user-search-result').on("click", '.user-search-add', function(){
+    // appendUserのカスタムデータ属性を使う(ここめっちゃ大事)
+    $(this).parent().remove();
+    var user_name = $(this).data("user-name")
+    var user_id   = $(this).data("user-id")
+    appendAddUser(user_name, user_id)
+  })
+  $('#chat-group-users').on("click", '.js-remove-btn', function(){
+    $(this).parent().remove();
   })
 })
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,8 +21,8 @@ class GroupsController < ApplicationController
   end
 
   def update
-    if @group.update
-      redirect_to grop_messages_path(@group), notice: 'グループを編集しました'
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
@@ -30,7 +30,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, { user_ids: [] })
   end
 
   def set_group
